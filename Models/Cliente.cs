ab6bfb88c2641f35973eda1f726131fff964dfcf
Sr Chordata <gustavoanjos945@gmail.com>
2021-10-13 19:02:26 -0300

    Cliente DAO & CRUD

---
+++ Models/Cliente.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DS_Sistelie.Models
+{
+    class Cadastrarcliente
+    {
+        public int CodigoCliente { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Rg { get; set; }
+
+        public string Cpf { get; set; }
+
+        public DateTime DataNascimento { get; set; }
+
+        public string Sexo { get; set; }
+
+        public string Telefone { get; set; }
+
+        public string Email { get; set; }
+
+        public string FkEndereco { get; set; }
+
+    }
+}
---
+++ Models/ClienteDAO.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DS_Sistelie.Interfaces;
+using DS_Sistelie.Database;
+using MySql.Data.MySqlClient;
+
+namespace DS_Sistelie.Models
+{
+    class ClienteDAO : IDAO<Cliente>
+    {
+        public void Delete(Cliente t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Cliente GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(Cliente t)
+        {
+            try
+            {
+                var query = conn.Query();
+                query.CommandText = "INSERT INTO Cliente (nome_clien, cpf_clien, rg_clien, datanasc_clien, sexo_clien, telefone_cli, email_clien, cod_endereco_fk) " +
+                    "VALUES (@nome, @cpf, @rg, @datanascimento, @sexo, @telefone, @email, @FkEndereco)";
+
+                query.Parameters.AddWithValue("@nome", t.Nome);
+                query.Parameters.AddWithValue("@cpf", t.Cpf);
+                query.Parameters.AddWithValue("@rg", t.Rg);
+                query.Parameters.AddWithValue("@datanascimento", t.DataNascimento.ToString("yyyy-MM-dd"));
+                query.Parameters.AddWithValue("@sexo", t.Sexo);
+                query.Parameters.AddWithValue("@telefone", t.Telefone);
+                query.Parameters.AddWithValue("@email", t.Email);
+                query.Parameters.AddWithValue("@FkEndereco", t.FkEndereco);
+
+                var result = query.ExecuteNonQuery();
+
+                if (result == 0)
+                {
+                    throw new Exception("O fornecedor não foi cadastrado, por favor tente novamente!");
+                }
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        public List<Cliente> List()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Cliente t)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
